49ea056df8327abd79ca121f1509f7ece96b4f3d
AhmedAAN <ahmed.a.nagy@outlook.com>
2023-04-17 09:23:56 -0700

    your task

---
+++ _strtok.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ */
+char *_strtok(char *buffer, const char *delimter)
+{
+	/* create a function that works like strtok()
+	 *
+	 * man for more info
+	 *
+	 * If buffer is not NULL, start fromthe begging of the buffer
+	 * but if buffer is null, the function starts from the last position you stopped at
+	 *
+	 * return value -----> The strtok() function return a pointer to the next token
+	 * , or NULL if there are no more tokens.
+	 */
+}
