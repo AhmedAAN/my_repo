e4e3de6066eb0f9227043dd2f887d4fd7cf086ad
Ahazzia <emmanuelbyiringiro9@gmail.com>
2023-04-18 08:04:54 -0400

    string tokenizer

--- _strtok.c
+++ _strtok.c
@@ -1,212 +1,94 @@
 #include "shell.h"
 
 /**
- * swap_char - swaps | and & for non-printed chars
- *
- * @input: input string
- * @bool: type of swap
- * Return: swapped string
+ * **strtow - splits a string into words. Repeat delimiters are ignored
+ * @str: the input string
+ * @d: the delimeter string
+ * Return: a pointer to an array of strings, or NULL on failure
  */
-char *swap_char(char *input, int bool)
+
+char **strtow(char *str, char *d)
 {
-	int i;
+	int i, j, k, m, numwords = 0;
+	char **s;
 
-	if (bool == 0)
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	if (!d)
+		d = " ";
+	for (i = 0; str[i] != '\0'; i++)
+		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
+			numwords++;
+
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < numwords; j++)
 	{
-		for (i = 0; input[i]; i++)
-		{
-			if (input[i] == '|')
-			{
-				if (input[i + 1] != '|')
-					input[i] = 16;
-				else
-					i++;
-			}
-
-			if (input[i] == '&')
-			{
-				if (input[i + 1] != '&')
-					input[i] = 12;
-				else
-					i++;
-			}
-		}
-	}
-	else
-	{
-		for (i = 0; input[i]; i++)
-		{
-			input[i] = (input[i] == 16 ? '|' : input[i]);
-			input[i] = (input[i] == 12 ? '&' : input[i]);
-		}
-	}
-	return (input);
-}
-
-/**
- * add_nodes - add separators and command lines in the lists
- *
- * @head_s: head of separator list
- * @head_l: head of command lines list
- * @input: input string
- * Return: no return
- */
-void add_nodes(sep_list **head_s, line_list **head_l, char *input)
-{
-	int i;
-	char *line;
-
-	input = swap_char(input, 0);
-
-	for (i = 0; input[i]; i++)
-	{
-		if (input[i] == ';')
-			add_sep_node_end(head_s, input[i]);
-
-		if (input[i] == '|' || input[i] == '&')
-		{
-			add_sep_node_end(head_s, input[i]);
+		while (is_delim(str[i], d))
 			i++;
+		k = 0;
+		while (!is_delim(str[i + k], d) && str[i + k])
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
+		{
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
 		}
+		for (m = 0; m < k; m++)
+			s[j][m] = str[i++];
+		s[j][m] = 0;
 	}
-
-	line = _strtok(input, ";|&");
-	do {
-		line = swap_char(line, 1);
-		add_line_node_end(head_l, line);
-		line = _strtok(NULL, ";|&");
-	} while (line != NULL);
-
+	s[j] = NULL;
+	return (s);
 }
 
 /**
- * go_next - go to the next command line stored
- *
- * @list_s: separator list
- * @list_l: command line list
- * @datash: data structure
- * Return: no return
+ * **strtow2 - splits a string into words
+ * @str: the input string
+ * @d: the delimeter
+ * Return: a pointer to an array of strings, or NULL on failure
  */
-void go_next(sep_list **list_s, line_list **list_l, data_shell *datash)
+char **strtow2(char *str, char d)
 {
-	int loop_sep;
-	sep_list *ls_s;
-	line_list *ls_l;
+	int i, j, k, m, numwords = 0;
+	char **s;
 
-	loop_sep = 1;
-	ls_s = *list_s;
-	ls_l = *list_l;
-
-	while (ls_s != NULL && loop_sep)
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		if ((str[i] != d && str[i + 1] == d) ||
+		    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
+			numwords++;
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < numwords; j++)
 	{
-		if (datash->status == 0)
+		while (str[i] == d && str[i] != d)
+			i++;
+		k = 0;
+		while (str[i + k] != d && str[i + k] && str[i + k] != d)
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
 		{
-			if (ls_s->separator == '&' || ls_s->separator == ';')
-				loop_sep = 0;
-			if (ls_s->separator == '|')
-				ls_l = ls_l->next, ls_s = ls_s->next;
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
 		}
-		else
-		{
-			if (ls_s->separator == '|' || ls_s->separator == ';')
-				loop_sep = 0;
-			if (ls_s->separator == '&')
-				ls_l = ls_l->next, ls_s = ls_s->next;
-		}
-		if (ls_s != NULL && !loop_sep)
-			ls_s = ls_s->next;
+		for (m = 0; m < k; m++)
+			s[j][m] = str[i++];
+		s[j][m] = 0;
 	}
-
-	*list_s = ls_s;
-	*list_l = ls_l;
-}
-
-/**
- * split_commands - splits command lines according to
- * the separators ;, | and &, and executes them
- *
- * @datash: data structure
- * @input: input string
- * Return: 0 to exit, 1 to continue
- */
-int split_commands(data_shell *datash, char *input)
-{
-
-	sep_list *head_s, *list_s;
-	line_list *head_l, *list_l;
-	int loop;
-
-	head_s = NULL;
-	head_l = NULL;
-
-	add_nodes(&head_s, &head_l, input);
-
-	list_s = head_s;
-	list_l = head_l;
-
-	while (list_l != NULL)
-	{
-		datash->input = list_l->line;
-		datash->args = split_line(datash->input);
-		loop = exec_line(datash);
-		free(datash->args);
-
-		if (loop == 0)
-			break;
-
-		go_next(&list_s, &list_l, datash);
-
-		if (list_l != NULL)
-			list_l = list_l->next;
-	}
-
-	free_sep_list(&head_s);
-	free_line_list(&head_l);
-
-	if (loop == 0)
-		return (0);
-	return (1);
-}
-
-/**
- * split_line - tokenizes the input string
- *
- * @input: input string.
- * Return: string splitted.
- */
-char **split_line(char *input)
-{
-	size_t bsize;
-	size_t i;
-	char **tokens;
-	char *token;
-
-	bsize = TOK_BUFSIZE;
-	tokens = malloc(sizeof(char *) * (bsize));
-	if (tokens == NULL)
-	{
-		write(STDERR_FILENO, ": allocation error\n", 18);
-		exit(EXIT_FAILURE);
-	}
-
-	token = _strtok(input, TOK_DELIM);
-	tokens[0] = token;
-
-	for (i = 1; token != NULL; i++)
-	{
-		if (i == bsize)
-		{
-			bsize += TOK_BUFSIZE;
-			tokens = _reallocdp(tokens, i, sizeof(char *) * bsize);
-			if (tokens == NULL)
-			{
-				write(STDERR_FILENO, ": allocation error\n", 18);
-				exit(EXIT_FAILURE);
-			}
-		}
-		token = _strtok(NULL, TOK_DELIM);
-		tokens[i] = token;
-	}
-
-	return (tokens);
+	s[j] = NULL;
+	return (s);
 }
